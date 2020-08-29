d93efca1766e7564ca5d961f2fe4638049e5c85c
m.muharram <msmaioali@gmail.com>
2020-08-29 20:01:34 +0300

    list sort commit

---
+++ src/Sortlist.kt
@@ -0,0 +1,9 @@
+fun main(){
+
+    val list = listOf(8,4,7,1,2,3,0,5,6)
+
+    println("List in ascending order" + list.sorted())
+    println("List in descending order" + list.sortedDescending())
+
+
+}
